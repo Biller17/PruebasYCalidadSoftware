7b06c73fa2aa73d0d191b9c43b9a076225f47b5d
Biller17 <abiller95@gmail.com>
2017-01-10 08:47:59 -0600

    Archivo con funcion recursiva

---
+++ TC3045/Parcial uno/Unitarias/first.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "first.h"
+
+int factorial(int number) {
+	int temp;
+
+	if(number <= 1) return 1;
+
+	temp = number * factorial(number - 1);
+	return temp;
+}
+
+void main(){
+}
